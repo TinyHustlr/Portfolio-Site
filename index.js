fb7203434499cb564f58ec105ff8de3dc94ddb9c
TinyHustlr <91208042+TinyHustlr@users.noreply.github.com>
2022-11-16 00:04:04 +1100

    Working home page draft
    Starting to come together

---
+++ index.js
@@ -0,0 +1,6 @@
+const modal = document.querySelector(".modal")
+const happyBtn = document.querySelector(".happy-btn")
+
+happyBtn.addEventListener("click", function() {
+    modal.style.display = "block"
+})
