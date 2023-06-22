09ac835f511b7b5966dfb96a68771952f1cfbfb5
TinyHustlr <91208042+TinyHustlr@users.noreply.github.com>
2023-06-22 16:42:54 +1000

    Blur effect working
    Needs CSS work for switch to keep it top right but dynamic

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const modal = document.querySelector(".modal")
-const happyBtn = document.querySelector(".happy-btn")
+const slideToggle = document.getElementById('slideToggle');
+const blurEffect = document.getElementById('blur');
 
-happyBtn.addEventListener("click", function() {
-    modal.style.display = "block"
-})
+slideToggle.addEventListener('change', function() {
+  if (this.checked) {
+    blurEffect.classList.remove('blureffect');
+    blurEffect.classList.add('blureffect-off');
+  } else {
+    blurEffect.classList.add('blureffect');
+    blurEffect.classList.remove('blureffect-off');
+  }
+});
