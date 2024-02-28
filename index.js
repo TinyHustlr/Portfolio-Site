67a1c09eb66cf119309e0c20a2c97f7a104ed0e1
David O'reilly <davidworeilly@gmail.com>
2024-02-28 11:46:49 +1100

    HTML and shopify logos added
    Swapped blur effect CSS to test

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const blurEffect = document.getElementById('blur');
 
 slideToggle.addEventListener('change', function() {
   if (this.checked) {
-    blurEffect.classList.remove('blureffect');
-    blurEffect.classList.add('blureffect-off');
-  } else {
     blurEffect.classList.add('blureffect');
     blurEffect.classList.remove('blureffect-off');
+  } else {
+    blurEffect.classList.remove('blureffect');
+    blurEffect.classList.add('blureffect-off');
   }
 });
